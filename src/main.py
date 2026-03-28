efadb6df42b5c78e1950c0446e040dd0a0e79233
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-29 03:26:55 +0530

    Added a lot to the training data also, really happy, I generated the ai model, now I need to test its accuracy.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 import fasttext
 import pandas as pd
 
@@ -6,9 +8,20 @@ def main():
     df = pd.read_parquet("training_data/training_data.parquet")
     with open("./output.train.txt", "w") as output_train_file:
         for i, j in zip(df["label"], df["readme_content"]):
+            j = j.replace("\n", " ").replace("\r", " ")
             output_train_file.write(i + " " + j + "\n")
 
-    print("Hello from threat-detection!")
+    model = fasttext.train_supervised(
+        input="./output.train.txt",
+        epoch=50,
+        lr=0.3,
+        wordNgrams=2,
+        dim=50,
+        minCount=1,
+        thread=os.cpu_count(),
+        verbose=2,
+    )
+    model.save_model("model.bin")
 
 
 if __name__ == "__main__":
