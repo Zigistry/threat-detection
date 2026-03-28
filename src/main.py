3dfe01c4e08bab917c1364f9d85e4cf34d402d9b
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-29 03:52:22 +0530

    wow! from the example data, I am getting 100% accuracy.

--- src/main.py
+++ src/main.py
@@ -4,13 +4,18 @@ import fasttext
 import pandas as pd
 
 
-def main():
-    df = pd.read_parquet("training_data/training_data.parquet")
-    with open("./output.train.txt", "w") as output_train_file:
+def paraquet_to_fasttext_kind_of_format(input_file, output_file):
+    df = pd.read_parquet(input_file)
+    with open(output_file, "w") as output_train_file:
         for i, j in zip(df["label"], df["readme_content"]):
             j = j.replace("\n", " ").replace("\r", " ")
             output_train_file.write(i + " " + j + "\n")
 
+
+def main():
+    paraquet_to_fasttext_kind_of_format(
+        "training_data/training_data.parquet", "output.train.txt"
+    )
     model = fasttext.train_supervised(
         input="./output.train.txt",
         epoch=50,
@@ -22,6 +27,14 @@ def main():
         verbose=2,
     )
     model.save_model("model.bin")
+
+    paraquet_to_fasttext_kind_of_format(
+        "training_data/testing_data.parquet", "output.test.txt"
+    )
+
+    results = model.test("output.test.txt")
+
+    print(results)
 
 
 if __name__ == "__main__":
