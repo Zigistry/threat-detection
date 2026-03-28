a678f9b98d7ba6ce56c721defbdf9edb4b6e121b
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-29 02:32:34 +0530

    Added basic logic regarding creating of the training file.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+import fasttext
+import pandas as pd
+
+
 def main():
+    df = pd.read_parquet("training_data/training_data.parquet")
+    with open("./output.train.txt", "w") as output_train_file:
+        for i, j in zip(df["label"], df["readme_content"]):
+            output_train_file.write(i + " " + j + "\n")
+
     print("Hello from threat-detection!")
 
 
