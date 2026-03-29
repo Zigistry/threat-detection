ee9cc76a7b75f84e4ff20c6ab4aa5d174e4b587a
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-30 03:03:26 +0530

    added url replacing algorithm

--- training_data/replacer.py
+++ training_data/replacer.py
@@ -17,7 +17,35 @@ def replace_github_and_codeberg_url(input_string):
     )
     input_string = re.sub(
         r"github/[^/\s]+/[^/\s]+(?:[^\s]*)?",
-        "https://example.com/example/example",
+        "example.com/example/example",
+        input_string
+    )
+
+    # replace any other with example.com/2
+    # I am doing this because,
+    # Afaik about ML algorithms, they learn patterns.
+    # So, it know like, what the url looks like.
+    # Hence:
+    # If github.com kind of url is there:
+    # it will think it like example.com
+    # And any other kind of url.
+    # It will think it like https://example.com/2/
+    # Why am I doing this? Because:
+    # Threat repos generally have their urls like:
+    # github.com/example/example/releases/malacious.zip
+    # And other urls can be like:
+    # img.shields.io/badge/something
+    # Thats why I want my ai to know that malacious
+    # urls generally look like github.com/example/example/releases/malacious.zip download,
+    # which will look like:
+    # https://example.com/
+    # And just to treat other img.shields.io/badge/something like different urls which will look like:
+    # https://example.com/2/"
+    # Also, I can implement this in next commit that the repos, I am scanning also go through
+    # this special tokenization for maximum accuracy, Maybe? I need to test this.
+    input_string = re.sub(
+        r"https?://(?!example\.com)[^\s]+",
+        "https://example.com/2/",
         input_string
     )
 
