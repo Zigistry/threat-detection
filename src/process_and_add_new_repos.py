f93298c35263d660ae3acf8e62a0f30b33e60245
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-29 16:08:12 +0530

    Working on processing and adding new repos

---
+++ src/process_and_add_new_repos.py
@@ -0,0 +1,17 @@
+# Basically this file will do this:
+# I need to read the index_new_repo table
+# then need to do a get request on their readme
+# then check using my spam detector algo, wether
+# the repo is safe to index or not.
+# if repo is safe:
+#     put it in repos table
+# if not:
+#     put it in quarantined_repos
+def main():
+  
+
+
+
+
+if __name__ == "__main__":
+  main()
