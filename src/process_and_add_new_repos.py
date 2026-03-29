795bea2f4d306233e36189c84c85aa35cbde7767
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-29 16:43:21 +0530

    Prediction is working and, I fixed the numpy version to work with fasttext and pandas

--- src/process_and_add_new_repos.py
+++ src/process_and_add_new_repos.py
@@ -7,14 +7,30 @@
 #     put it in repos table
 # if not:
 #     put it in quarantined_repos
-import libsql
 import os
+
+import fasttext
+import libsql
+import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+
+def process_github_repo(owner_name, repo_name, model):
+    url_to_process = f"https://api.github.com/repos/{owner_name}/{repo_name}/readme"
+    headers = {"Accept": "application/vnd.github.v3.raw"}
+    res = requests.get(url_to_process, headers=headers)
+
+    text_to_process = res.text.replace("\n", " ").replace("\r", " ")
+    print(model.predict(text_to_process))
+
+
+def process_codeberg_repo(owner_name, repo_name):
+    pass
 
 
 def main():
+    load_dotenv()
+    model = fasttext.load_model("model.bin")
     connection = libsql.connect(
         database="zigistry.db",
         sync_url=os.getenv("DATABASE_URL"),
@@ -26,7 +42,12 @@ def main():
     rows = cursor.fetchall()
 
     for row in rows:
-        print(row)
+        provider, owner_name, repo_name = row[0].split("/")
+        if provider == "gh":
+            process_github_repo(owner_name, repo_name, model)
+        else:
+            process_codeberg_repo(owner_name, repo_name)
+
 
 if __name__ == "__main__":
     main()
