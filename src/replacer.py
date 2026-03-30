9feb86be53b8caad63179ef3e9b611568570d52d
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-30 16:18:43 +0530

    removed the parquet fixing lines, now it is fixed already.

--- src/replacer.py
+++ src/replacer.py
@@ -7,9 +7,7 @@ def process_file(file_name):
     df.loc[:, "readme_content"] = df["readme_content"].map(
         replace_github_and_codeberg_url
     )
-    df.loc[:, "if_is_valid_repo"] = df["if_is_valid_repo"].map(
-        lambda x: True if x == "True" else False
-    )
+
     df.to_parquet(file_name, index=False)
 
 process_file("training_data/testing_data.parquet")
