40b86f546dce3c4afeda821f5506138297716619
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-03-30 17:31:19 +0530

    fixed a lot of bugs, like, not correctly checking  res.text, and passing model when model was not used and DELETE FROM instead of DELETE * FROM

--- src/process_and_add_new_repos.py
+++ src/process_and_add_new_repos.py
@@ -14,22 +14,23 @@ import fasttext
 import libsql
 import requests
 from dotenv import load_dotenv
+
 from tokenizer import replace_github_and_codeberg_url
 
 
 def check_if_text_good(text, model):
     text = text.replace("\n", " ").replace("\r", " ")
     text = replace_github_and_codeberg_url(text)
-    return model.predict(text)[0] == ('__label__good',)
+    return model.predict(text)[0] == ("__label__good",)
 
 
-def process_github_repo(owner_name, repo_name, model):
+def process_github_repo(owner_name, repo_name):
     url_to_process = f"https://api.github.com/repos/{owner_name}/{repo_name}/readme"
     headers = {
         "Accept": "application/vnd.github.v3.raw",
         "Authorization": f"Bearer {os.getenv('GH_API_KEY')}",
     }
-    res = requests.get(url_to_process, headers=headers)
+    res = requests.get(url_to_process, headers=headers, timeout=10)
 
     if res.status_code == 404:
         return ""
@@ -41,14 +42,19 @@ def process_github_repo(owner_name, repo_name, model):
 
 
 def process_codeberg_repo(owner_name, repo_name):
-    url_to_process = f"https://codeberg.org/{owner_name}/{repo_name}/raw/branch/main/README.md"
-    res = requests.get(url_to_process)
+    url_to_process = (
+        f"https://codeberg.org/{owner_name}/{repo_name}/raw/branch/main/README.md"
+    )
+    res = requests.get(url_to_process, timeout=10)
 
     if res.status_code == 404:
         return ""
 
     if res.status_code != 200:
         sys.exit(1)
+
+    if not res.text:
+        return ""
 
     return res.text
 
@@ -70,16 +76,25 @@ def main():
         provider, owner_name, repo_name = row[0].split("/")
         readme_text = ""
         if provider == "gh":
-            readme_text = process_github_repo(owner_name, repo_name, model)
+            readme_text = process_github_repo(owner_name, repo_name)
         else:
             readme_text = process_codeberg_repo(owner_name, repo_name)
 
         if readme_text == "" or check_if_text_good(readme_text, model):
-            cursor.execute("INSERT INTO safe_to_index_new_repo VALUES (?, ?, ?)", (provider, owner_name, repo_name))
+            cursor.execute(
+                "INSERT OR IGNORE INTO safe_to_index_new_repo VALUES (?, ?, ?)",
+                (provider, owner_name, repo_name),
+            )
         else:
-            cursor.execute("INSERT INTO quarantined_repos VALUES (?, ?, ?)", (provider, owner_name, repo_name))
+            cursor.execute(
+                "INSERT OR IGNORE INTO quarantined_repos VALUES (?, ?, ?)",
+                (provider, owner_name, repo_name),
+            )
 
-    cursor.execute("DELETE * FROM index_new_repo")
+    cursor.execute("DELETE FROM index_new_repo")
+
+    connection.commit()
+
 
 if __name__ == "__main__":
     main()
